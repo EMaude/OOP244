8afbd2eb4e96625fac3e2787417670a8640977b4
Elliot Maude <maude.elliot@gmail.com>
2017-10-10 13:31:18 -0700

    ws4 - presubmit

--- Workshops/WS04/at-home/Passenger.cpp
+++ Workshops/WS04/at-home/Passenger.cpp
@@ -34,6 +34,30 @@ namespace sict
 			strcpy(m_dest, dest);
 		}
 
+		m_year = 2017;
+		m_month = 7;
+		m_day = 1;
+
+	}
+
+	Passenger::Passenger(const char * name, const char * dest, int year, int month, int day)
+	{
+		*this = Passenger(name, dest);
+	
+		if (2017 <= year && year <= 2020 && 1 <= month && month <= 12 && 1 <= day && day <= 31)
+		{
+			m_year = year;
+			m_month = month;
+			m_day = day;
+		}
+		else
+		{	//Safe Empty state
+			strcpy(m_name, "");
+			strcpy(m_dest, "");
+			m_year = 2017;
+			m_month = 7;
+			m_day = 1;
+		}
 	}
 
 	Passenger::~Passenger()
@@ -44,6 +68,7 @@ namespace sict
 	{
 		return (strcmp(m_name, "") == 0 || strcmp(m_dest, "") == 0) ? true : false;
 	}
+
 	void Passenger::display() const
 	{
 		if (isEmpty())
@@ -51,8 +76,39 @@ namespace sict
 			cout << "No passenger!" << endl;
 		}
 		else
+		{	
+			cout.precision(2);
+			cout << m_name << " - " << m_dest << " on " << m_year << "/" << m_month << "/" << m_day << endl;
+		}
+	}
+
+	const char* Passenger::name() const
+	{
+		if (isEmpty())
 		{
-			cout << m_name << " - " << m_dest << endl;
+			return "";
+		}
+		else
+		{
+			return m_name;
+		}
+	}
+
+	bool Passenger::canTravelWith(const Passenger& pass) const
+	{
+		if (pass.isEmpty())
+		{
+			return false;
+		}
+		else
+		{
+			if (pass.m_dest == m_dest)
+			{
+				if (pass.m_year == m_year && pass.m_month == m_month && pass.m_day == m_day)
+				{
+					return true;
+				}
+			}
 		}
 	}
 }
--- Workshops/WS04/at-home/Passenger.h
+++ Workshops/WS04/at-home/Passenger.h
@@ -8,13 +8,19 @@ namespace sict
 	public:
 		Passenger();
 		Passenger(const char* name,const char* dest);
+		Passenger(const char* name, const char* dest, int year, int month, int day);
 		~Passenger();
 		bool isEmpty() const;
 		void display() const;
+		const char* name() const;
+		bool canTravelWith(const Passenger&) const;
 
 	private:
 		char m_name[32];
 		char m_dest[32];
+		int m_year;
+		int m_month;
+		int m_day;
 	};
 }
 #endif // !SICT_PASSENGER_H
